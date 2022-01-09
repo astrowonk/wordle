4dbc54ea21c89078e73f5afd39cf9663a721abb1
Marcos Huerta <sirmarcos@gmail.com>
2022-01-09 16:59:47 -0500

    remove init word re spoilers

--- tweet_script.py
+++ tweet_script.py
@@ -1,6 +1,10 @@
 from wordle import WordNetWordle
 import argparse
 import logging
+from config import *
+import tweepy
+import json
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Wordle')
@@ -14,9 +18,33 @@ if __name__ == '__main__':
                         filename=log_file,
                         level='DEBUG')
 
-    w = WordNetWordle()
-    score, word, text, luck = w.play_game(args.target_word,
-                                          args.wordle_num,
-                                          force_init_guess='lares')
+    try:
+        with open('history.json') as f:
+            history = json.load(f)
+    except:
+        history = {}
+    api = tweepy.Client(consumer_key=api_key,
+                        consumer_secret=api_secret,
+                        access_token=access_token,
+                        access_token_secret=access_token_secret)
 
-    print(text)
+    if args.wordle_num not in history:
+        w = WordNetWordle()
+        score, word, text, luck = w.play_game(args.target_word,
+                                              args.wordle_num,
+                                              force_init_guess=initial_guess)
+
+        response = api.create_tweet(text=text)
+
+        history.update({
+            args.wordle_num: {
+                'id': response.data['id'],
+                'score': score,
+                'word': word,
+                'text': text,
+                'luck': luck
+            }
+        })
+        with open('history.json', 'w') as f:
+            history = json.dump(history, f, indent=4)
+    ## need to make code to check date and tweet log reply to yesterday as well as upload the log?
