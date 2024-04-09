16adbea00cac0eaa70bc474b96f1183ee0d0de93
Marcos Huerta <marcos@marcoshuerta.com>
2024-04-09 07:47:56 -0400

    handle missing solution in target list

--- tweet_script.py
+++ tweet_script.py
@@ -3,38 +3,20 @@ import argparse
 import requests
 import datetime
 import logging
-from config import *
+from config import initial_guess
 import json
 from mastodon import Mastodon
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='Wordle')
-    parser.add_argument('target_word',
-                        type=str,
-                        help='target word',
-                        nargs='?',
-                        default=None)
-    parser.add_argument('wordle_num',
-                        type=int,
-                        help='number of wordle',
-                        nargs='?',
-                        default=None)
-    parser.add_argument('--no-tweet',
-                        action='store_true',
-                        help='no tweet',
-                        default=False)
-    parser.add_argument('--no-mast',
-                        action='store_true',
-                        help='no mastodon',
-                        default=False)
+    parser.add_argument('target_word', type=str, help='target word', nargs='?', default=None)
+    parser.add_argument('wordle_num', type=int, help='number of wordle', nargs='?', default=None)
+    parser.add_argument('--no-tweet', action='store_true', help='no tweet', default=False)
+    parser.add_argument('--no-mast', action='store_true', help='no mastodon', default=False)
 
-    parser.add_argument('--date',
-                        type=str,
-                        help='run for this date',
-                        default=None)
+    parser.add_argument('--date', type=str, help='run for this date', default=None)
     args = parser.parse_args()
-    log_file = f"wordle_{args.wordle_num}.txt"
+    log_file = f'wordle_{args.wordle_num}.txt'
 
     # logging.basicConfig(format='%(asctime)s %(message)s',
     #                     datefmt='%m/%d/%Y %I:%M:%S %p',
@@ -48,11 +30,11 @@ if __name__ == '__main__':
         history = []
     if not (args.target_word and args.wordle_num):
         if not args.date:
-            date = datetime.datetime.now().strftime("%Y-%m-%d")
+            date = datetime.datetime.now().strftime('%Y-%m-%d')
         else:
             date = args.date
-        url = f"https://www.nytimes.com/svc/wordle/v2/{date}.json"
-        print(f"Retrieving {url}")
+        url = f'https://www.nytimes.com/svc/wordle/v2/{date}.json'
+        print(f'Retrieving {url}')
         data = requests.get(url).json()
         wordle_num = data['days_since_launch']
         target_word = data['solution']
@@ -61,8 +43,17 @@ if __name__ == '__main__':
         wordle_num = args.wordle_num
 
     w = WordNetWordle2(log_file=log_file)
-    score, word, text, luck, word_list = w.play_game(
-        target_word, wordle_num, force_init_guess=initial_guess)
+    try:
+        score, word, text, luck, word_list = w.play_game(
+            target_word, wordle_num, force_init_guess=initial_guess
+        )
+    except AssertionError:
+        w.target_words = w.short_words
+        score, word, text, luck, word_list = w.play_game(
+            target_word, wordle_num, force_init_guess=initial_guess
+        )
+
+        pass
     print('solved')
     w.logger.setLevel(logging.CRITICAL)
     entry = [x for x in history if x['wordle_num'] == wordle_num]
@@ -71,28 +62,26 @@ if __name__ == '__main__':
     else:
         entry = {}
     if not args.no_mast:
+        assert entry.get('mast_id') is None, f'wordle {wordle_num} has been tooted'
 
-        assert entry.get(
-            'mast_id') is None, f'wordle {wordle_num} has been tooted'
-
-        text = text + "\n#Wordle"
-        mastodon = Mastodon(access_token='mastodon.secret',
-                            api_base_url='https://botsin.space')
-        response = mastodon.status_post(status=text,
-                                        spoiler_text=f"Wordle {wordle_num}")
+        text = text + '\n#Wordle'
+        mastodon = Mastodon(access_token='mastodon.secret', api_base_url='https://botsin.space')
+        response = mastodon.status_post(status=text, spoiler_text=f'Wordle {wordle_num}')
         mastodon_id = response['id']
     else:
         mastodon_id = None
 
-    history.append({
-        'wordle_num': wordle_num,
-        'masto_id': mastodon_id,
-        'score': score,
-        'word': word,
-        'text': text,
-        'luck': luck,
-        'word_list': word_list
-    })
+    history.append(
+        {
+            'wordle_num': wordle_num,
+            'masto_id': mastodon_id,
+            'score': score,
+            'word': word,
+            'text': text,
+            'luck': luck,
+            'word_list': word_list,
+        }
+    )
     with open('better_history.json', 'w') as f:
         json.dump(history, f, indent=4)
 
